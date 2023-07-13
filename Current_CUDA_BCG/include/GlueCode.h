9535bf7a0e8ecc10138451b7b0d3e13567276f13
bashirad <79177010+bashirad@users.noreply.github.com>
2023-07-13 10:40:42 -0400

    GlueCode added

---
+++ Current_CUDA_BCG/include/GlueCode.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/// @brief strategize converts a population into a strategy.
+/// @param populuation that contains individuals of strategies.
+/// @return a strategy individual of the population one at a time.
+Strategy strategize(Population populuation);
+
+/// @brief popularize converts a strategy into a population.
+/// @param strategy that will be combined into a group of individuals or population.
+/// @return a population that contains the strategies.
+Population popularize (Strategy strategy);
